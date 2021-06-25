48f2eda30429100c13f84920998aa4f2ae50a278
czechb3 <bartosz.czech@contractors.roche.com>
2021-06-25 13:24:26 +0200

    Import gDRimport package

---
+++ R/import_data.R
@@ -0,0 +1,28 @@
+#' Import raw data
+#'
+#' @param manifest_file character, file path(s) to manifest(s)
+#' @param template_file data.frame, with datapaths and names of results file(s)
+#' or character with file path of templates file(s)
+#' @param results_file data.frame, with datapaths and names of results file(s)
+#' or character with file path of results file(s)
+#' @param instrument character with type of instrument used
+#' @param collapse_Drugs
+#'
+#' @return
+#' @export
+#'
+#' @examples
+import_data <- function(manifest_file,
+                        template_file,
+                        results_file,
+                        instrument = "EnVision",
+                        collapse_Drugs = TRUE) {
+  loaded_data <- load_data(manifest_file = manifest_file,
+                           df_template_files= template_file,
+                           results_file = results_file,
+                           instrument = instrument)
+  merge_data(manifest = loaded_data$manifest,
+             treatments = loaded_data$treatments,
+             data = loaded_data$data,
+             collapse_Drugs = collapse_Drugs)
+}
--- R/package.R
+++ R/package.R
@@ -1,3 +1,4 @@
 #' @importFrom gDRutils fit_curves logisticFit get_header reset_headers get_identifier set_identifier reset_identifiers convert_se_assay_to_dt validate_SE flatten split_SE_components
 #' @importFrom gDRcore merge_data runDrugResponseProcessingPipeline create_SE normalize_SE average_SE fit_SE
+#' @importFrom gDRimport load_data
 NULL
