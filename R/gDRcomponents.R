e1ea15431b6ae0e96bf0d67d95b7d22a7afd2c2a
Bartosz Czech <bartosz.czech@contractors.roche.com>
2021-07-28 14:23:10 +0200

    feat: export gDRcomponents functions in gDR

---
+++ R/gDRcomponents.R
@@ -0,0 +1,14 @@
+#' @export
+gDRcomponents::getDrugs
+
+#' @export
+gDRcomponents::getCellLines
+
+#' @export
+gDRcomponents::getProjects
+
+#' @export
+gDRcomponents::getMetaProjects
+
+#' @export
+gDRcomponents::pullProjectSE
