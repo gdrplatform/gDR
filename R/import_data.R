44a9ea30b6fd220dfcbd2e5fada12d90f445f450
Bartosz Czech <bartosz.czech@contractors.roche.com>
2022-04-13 14:52:46 +0200

    refactor: remove redundant function argument

--- R/import_data.R
+++ R/import_data.R
@@ -6,7 +6,6 @@
 #' @param results_file data.frame, with datapaths and names of results file(s)
 #' or character with file path of results file(s)
 #' @param instrument character with type of instrument used
-#' @param collapse_Drugs
 #'
 #' @return
 #' @export
@@ -15,14 +14,12 @@
 import_data <- function(manifest_file,
                         template_file,
                         results_file,
-                        instrument = "EnVision",
-                        collapse_Drugs = TRUE) {
+                        instrument = "EnVision") {
   loaded_data <- load_data(manifest_file = manifest_file,
                            df_template_files = template_file,
                            results_file = results_file,
                            instrument = instrument)
   merge_data(manifest = loaded_data$manifest,
              treatments = loaded_data$treatments,
-             data = loaded_data$data,
-             collapse_Drugs = collapse_Drugs)
+             data = loaded_data$data)
 }
