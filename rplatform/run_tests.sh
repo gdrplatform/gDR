7185e55fa10c3c3fa389502b468f31981be74f84
Arkadiusz Gladki <arkadiusz.gladki@contractors.roche.com>
2023-02-28 14:03:36 +0100

    test: use function from gDRstyle to conduct tests/checks/lintering

--- rplatform/run_tests.sh
+++ rplatform/run_tests.sh
@@ -1,29 +1,13 @@
 #/bin/sh
+repo_path="$1"
 
 echo "Executing $0"
 echo "Environment: ${rp_env}"
 echo "Working directory: `pwd`"
 echo "Working directory contains: `ls | tr '\n' ' '`"
 
-# exit when any command fails (including any in pipes)
-set -eo pipefail
+# exit when any command fails
+set -e
 
-CORE_DIR=/mnt/vol
-echo ">>>>> OWNER OF THE REPOSITORY"
-# Fix for the access permissions issues with 'R CMD' when running Docker image with 'USER 1000'
-sudo chown -R `id -u` $CORE_DIR
-
-echo ">>>>>>>> RUNNING LINT"
-Rscript -e "gDRstyle::lintPkgDirs('/mnt/vol')"
-
-echo ">>>>> RUNNING UNIT TESTS"
-Rscript -e "testthat::test_local(path = '/mnt/vol', stop_on_failure = TRUE) "
-
-echo ">>>>> RUNNING CHECK"
-# add _R_CHECK_FORCE_SUGGESTS_ as a FALSE, to prevend error in check
-export _R_CHECK_FORCE_SUGGESTS_=0
-R CMD build /mnt/vol 
-R CMD check gDR_*.tar.gz --no-vignettes --no-examples --no-manual
-
-echo ">>>>>>>> RUNNING CHECK DEPENDENCIES"
-Rscript -e "gDRstyle::checkDependencies(desc_path='/mnt/vol/DESCRIPTION', dep_path='/mnt/vol/rplatform/dependencies.yaml')"
+echo ">>>>>>>> RUNNING CHECK"
+Rscript -e "gDRstyle::checkPackage('gDR', '$repo_path', 'gDR', 'error')"
