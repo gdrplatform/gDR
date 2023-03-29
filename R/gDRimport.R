0cd94b2c2501b65f9c7fd8fa7bcf59951c6e9607
Arkadiusz Gladki <arkadiusz.gladki@contractors.roche.com>
2023-03-29 20:05:34 +0200

    feat: do not reexport gdrplatform packages

--- R/gDRimport.R
+++
@@ -1,3 +0,0 @@
-#' @export
-gDRimport::load_data
-
--- R/package.R
+++ R/package.R
@@ -1,19 +1,1 @@
-#' @importFrom gDRutils fit_curves 
-#' @importFrom gDRutils logisticFit 
-#' @importFrom gDRutils get_header 
-#' @importFrom gDRutils get_env_identifiers 
-#' @importFrom gDRutils get_SE_identifiers 
-#' @importFrom gDRutils set_env_identifier 
-#' @importFrom gDRutils set_SE_identifiers 
-#' @importFrom gDRutils reset_env_identifiers 
-#' @importFrom gDRutils convert_se_assay_to_dt 
-#' @importFrom gDRutils validate_SE 
-#' @importFrom gDRutils flatten 
-#' @importFrom gDRutils split_SE_components
-#' @importFrom gDRcore merge_data 
-#' @importFrom gDRcore runDrugResponseProcessingPipeline 
-#' @importFrom gDRcore normalize_SE average_SE 
-#' @importFrom gDRcore fit_SE 
-#' @importFrom gDRcore create_and_normalize_SE
-#' @importFrom gDRimport load_data
 NULL
