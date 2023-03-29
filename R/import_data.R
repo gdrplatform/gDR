2281dff37230f87506708b5d32eb050d6f645aec
Arkadiusz Gladki <arkadiusz.gladki@contractors.roche.com>
2023-03-29 20:06:28 +0200

    doc: update vignette
    Details:
     - added namespacing
     - use gDRimport::get_test_data
     - minor fixes

--- R/import_data.R
+++ R/import_data.R
@@ -7,6 +7,10 @@
 #' or character with file path of results file(s)
 #' @param instrument character with type of instrument used
 #' 
+#' @examples
+#' td <- gDRimport::get_test_data()
+#' i_df <- import_data(td$m_file, td$t_files, td$r_files)
+#' 
 #' @return a \code{data.frame}
 #'
 #' @export
@@ -15,11 +19,11 @@ import_data <- function(manifest_file,
                         template_file,
                         results_file,
                         instrument = "EnVision") {
-  loaded_data <- load_data(manifest_file = manifest_file,
+  loaded_data <- gDRimport::load_data(manifest_file = manifest_file,
                            df_template_files = template_file,
                            results_file = results_file,
                            instrument = instrument)
-  merge_data(manifest = loaded_data$manifest,
+  gDRcore::merge_data(manifest = loaded_data$manifest,
              treatments = loaded_data$treatments,
              data = loaded_data$data)
 }
