efe7aa5af39426b6539ce3de3a85d969c705cb82
Janina Smola <janina.smola@contractors.roche.com>
2024-05-08 13:35:52 +0200

    chore: update puml img

--- inst/umls/refresh.sh
+++ inst/umls/refresh.sh
@@ -1,7 +1,10 @@
 # sudo apt-get install plantuml
 # pretty old version
 #jar_path="/usr/share/platinuml/platinuml.jar"
-# https://github.com/plantuml/plantuml/releases/download/v1.2023.13/plantuml-1.2023.13.jar
 jar_path="./plantuml-1.2023.13.jar"
-#java -Djava.awt.headless=true -jar $jar_path *.puml -tsvg -o ../images/
+if [ -f $jar_path ]; then
+   echo "File $jar_path exists."
+else
+   wget https://github.com/plantuml/plantuml/releases/download/v1.2023.13/plantuml-1.2023.13.jar
+fi
 java -Djava.awt.headless=true -jar $jar_path *.puml -tpng -o ../images/
