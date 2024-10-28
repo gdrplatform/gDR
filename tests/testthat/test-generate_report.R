fb881ae4e79f8c0abaf17e2ba74a2f00d61e8e65
czechb3 <bartosz.w.czech@gmail.com>
2024-10-28 09:08:15 +0100

    test: add unit tests

---
+++ tests/testthat/test-generate_report.R
@@ -0,0 +1,19 @@
+test_that("is_null_or_empty function works correctly", {
+  expect_true(is_null_or_empty(NULL))
+  expect_true(is_null_or_empty(""))
+  expect_true(is_null_or_empty(c("", "test")))
+  expect_false(is_null_or_empty("test"))
+  expect_false(is_null_or_empty(1:5))
+})
+
+test_that("run_gDR_with_report function handles input correctly", {
+  expect_error(run_gDR_with_report(output_dir = "~/test_report",
+                                   manifest = "path/to/manifest",
+                                   treatment = "path/to/treatment"),
+               "Provide either qcs_id or manifest, treatment, and raw_data, or prism_data_path.")
+  expect_error(run_gDR_with_report(output_dir = "~/test_report",
+                                   manifest = "path/to/manifest",
+                                   treatment = "path/to/treatment",
+                                   raw_data = "path/to_raw_data"),
+               "'manifest_file' must be a readable path")
+})
