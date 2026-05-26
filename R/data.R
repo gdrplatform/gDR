a037055172b9425e574d68f016b8040894b4b126
Bartek Czech <bartosz.w.czech@gmail.com>
2026-05-26 10:42:59 +0200

    fix: correct typos in documentation

--- R/data.R
+++ R/data.R
@@ -1,7 +1,7 @@
 #' Small data.table with raw data used for processing via gDR
 #'
 #' A dataset containing the ReadoutValues for single-agent experiments
-#' made-up of 10 drugs and 10 cell lines
+#' made up of 10 drugs and 10 cell lines
 #'
 #' @format A data frame with 3300 rows and 12 variables:
 #' \describe{
@@ -27,7 +27,7 @@
 
 #' Small data.table with raw combo data used for processing via gDR
 #'
-#' A dataset containing the ReadoutValues for combo experiments made-up of
+#' A dataset containing the ReadoutValues for combo experiments made up of
 #' 3 drugs, 2 co-drugs, and 2 cell lines
 #'
 #' @format A data frame with 3600 rows and 16 variables:
